a12fadf69b5cf5150c6cfe528da88f64ac30d032
umarbek <kinddevs2024@gmail.com>
2025-02-09 22:30:49 +0500

    start

---
+++ src/pages/Eror-404/Eror.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Eror = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <img className='w-[70%] h-[50%]' src="public/404.svg" alt="404" />
+    </div>
+  )
+}
+
+export default Eror;
